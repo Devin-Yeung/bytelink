01f0ecb1abf73f9edb6c8ba66c90fd690e44aa5f
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-10-20 03:54:00 +0800

    feat: defining genesis

---
+++ src/genesis.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::path::Path;
+use anyhow::Result;
+use jiff::Timestamp;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Genesis {
+    date: Timestamp,
+    /// The unique identifier for this running instance of the blockchain.
+    chain_id: u16,
+    /// The maximum number of transactions allowed in each block.
+    transections_per_block: u16,
+    /// The difficulty level for mining new blocks.
+    difficulty: u16,
+    /// The reward given to miners for successfully mining a block.
+    mining_reward: u64,
+    /// The price of gas for executing transactions.
+    gas_price: u64,
+    /// A mapping of account addresses to their initial balances.
+    balances: HashMap<String, u64>,
+}
+
+impl Genesis {
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Genesis> {
+        let content = std::fs::read_to_string(path)?;
+        let genesis: Genesis = serde_json::from_str(&content)?;
+        Ok(genesis)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::genesis::Genesis;
+
+    #[test]
+    fn load_genesis() {
+        let genesis = Genesis::load("assets/genesis.json").unwrap();
+        insta::assert_json_snapshot!(genesis);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod genesis;
+
 fn main() {
     println!("Hello, world!");
 }
