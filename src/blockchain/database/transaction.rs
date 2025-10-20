a09f3dc254e13041c9c391ab2ee7b555098591fc
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-10-20 22:41:18 +0800

    refactor: refine tx and signed tx

--- src/blockchain/database/transaction.rs
+++ src/blockchain/database/transaction.rs
@@ -21,15 +21,98 @@ pub struct Transaction {
 }
 
 impl Transaction {
-    pub fn sign<S: SignerSync, E: Fn(&Transaction) -> Vec<u8>>(
-        self,
-        signer: S,
-        encoder: E,
-    ) -> Result<Signature> {
-        let encoded = encoder(&self);
+    pub fn sign_by<S: SignerSync>(self, signer: &S) -> Result<SignedTx> {
+        let encoded = serde_json::to_vec(&self)?;
         let signature = signer.eip191_sign_msg(&encoded)?;
-        Ok(signature)
+        Ok(SignedTx {
+            transaction: self,
+            signature,
+        })
     }
 }
 
-pub struct TxBlock {}
+pub struct SignedTx {
+    pub transaction: Transaction,
+    pub signature: Signature,
+}
+
+impl SignedTx {
+    pub fn verify(&self) -> Result<()> {
+        let encoded = serde_json::to_vec(&self.transaction)?;
+        let recovered_address = self.signature.recover_address_from_msg(&encoded)?;
+
+        if recovered_address != self.transaction.from {
+            anyhow::bail!(
+                "Invalid transaction signature: recovered address does not match sender address"
+            );
+        }
+
+        if recovered_address == self.transaction.to {
+            anyhow::bail!("Invalid transaction: sender and recipient addresses cannot be the same");
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::crypto::signer::{Signer, SignerSync};
+
+    #[test]
+    pub fn naive_valid_tx() {
+        let alice = Signer::random();
+        let bob = Signer::random();
+
+        let tx = crate::blockchain::database::transaction::Transaction {
+            nonce: 0,
+            from: alice.address(),
+            to: bob.address(),
+            value: 100,
+            tip: 1,
+            data: vec![],
+        };
+
+        let signed_tx = tx.sign_by(&alice).unwrap();
+
+        assert!(signed_tx.verify().is_ok());
+    }
+
+    #[test]
+    pub fn send_to_self_invalid_tx() {
+        let alice = Signer::random();
+
+        let tx = crate::blockchain::database::transaction::Transaction {
+            nonce: 0,
+            from: alice.address(),
+            to: alice.address(),
+            value: 100,
+            tip: 1,
+            data: vec![],
+        };
+
+        let signed_tx = tx.sign_by(&alice).unwrap();
+
+        assert!(signed_tx.verify().is_err());
+    }
+
+    #[test]
+    pub fn bad_signature_invalid_tx() {
+        let alice = Signer::random();
+        let bob = Signer::random();
+        let charlie = Signer::random();
+
+        let tx = crate::blockchain::database::transaction::Transaction {
+            nonce: 0,
+            from: alice.address(),
+            to: bob.address(),
+            value: 100,
+            tip: 1,
+            data: vec![],
+        };
+
+        let signed_tx = tx.sign_by(&charlie).unwrap();
+
+        assert!(signed_tx.verify().is_err());
+    }
+}
