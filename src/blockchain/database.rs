0e80258b0507bb86d2a6e97681856fc7043c81d4
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-10-22 16:31:06 +0800

    feat: turn database to concurrent safe

--- src/blockchain/database.rs
+++ src/blockchain/database.rs
@@ -1,19 +1,21 @@
 use crate::blockchain::database::account::{Account, Address};
 use crate::blockchain::genesis::Genesis;
 use anyhow::Result;
-use std::collections::HashMap;
+use dashmap::DashMap;
+use dashmap::mapref::one::Ref;
 
 pub mod account;
 pub mod transaction;
 
+/// The in-memory, concurrent safe database for blockchain accounts.
 pub struct Database {
     genesis: Genesis,
-    accounts: HashMap<Address, Account>,
+    accounts: DashMap<Address, Account>,
 }
 
 impl Database {
     pub fn new(genesis: Genesis) -> Result<Database> {
-        let mut accounts = HashMap::new();
+        let accounts = DashMap::new();
 
         for (addr, balance) in &genesis.balances {
             let address = Address::parse_checksummed(addr)?;
@@ -24,7 +26,7 @@ impl Database {
         Ok(Database { genesis, accounts })
     }
 
-    pub fn query<T: AsRef<Address>>(&self, address: T) -> Option<&Account> {
+    pub fn query<T: AsRef<Address>>(&self, address: T) -> Option<Ref<'_, Address, Account>> {
         self.accounts.get(address.as_ref())
     }
 }
